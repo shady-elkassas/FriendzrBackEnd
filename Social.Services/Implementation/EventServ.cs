641cba37eafafee012a0bf23971eb24bfd74bd16
Alaa Farag <aabdelghany@youxel.com>
2023-01-29 16:22:02 +0200

    fix issue in date filter allow white lable events

--- Social.Services/Implementation/EventServ.cs
+++ Social.Services/Implementation/EventServ.cs
@@ -780,16 +780,10 @@ namespace Social.Services.Implementation
 
             List<EventData> data = EventChatAttends.Where(m => !blockod.Contains(m.EventDataid)).Where(m => m.EventData.eventdateto.Value.Date >= DateTime.Now.Date && m.EventData.IsActive == true && (m.EventData.StopFrom != null ? (m.EventData.StopFrom.Value >= DateTime.Now.Date || m.EventData.StopTo.Value <= DateTime.Now.Date) : true) && (m.EventData.EventTypeList.key == true ? (m.UserattendId == id && m.stutus != 1 && m.stutus != 2) : true)).Select(m => m.EventData).Distinct().ToList();
 
-            if (startDate != null)
+            if (startDate != null && endDate != null)
             {
 
-                data = data.Where(q => q.eventdate?.Date >= startDate).ToList();
-
-            }
-            if (endDate != null)
-            {
-
-                data = data.Where(q => q.eventdateto?.Date <= endDate).ToList();
+                data = data.Where(q => q.eventdate?.Date >= startDate || q.eventdateto?.Date <= endDate).ToList();
 
             }
 
@@ -1161,16 +1155,10 @@ namespace Social.Services.Implementation
 
             var eventDataList = eventChatAttendList.Where(m => !blockedEventIds.Contains(m.EventDataid)).Where(m => m.EventData.eventdateto.Value.Date >= DateTime.UtcNow.Date).Select(m => m.EventData).Distinct().ToList();
 
-            if (startDate != null)
+            if (startDate != null && endDate !=null)
             {
 
-                eventDataList = eventDataList.Where(q => q.eventdate?.Date >= startDate).ToList();
-
-            }
-            if (endDate != null)
-            {
-
-                eventDataList = eventDataList.Where(q => q.eventdateto?.Date <= endDate).ToList();
+                eventDataList = eventDataList.Where(q => q.eventdate?.Date >= startDate  || q.eventdateto?.Date <= endDate).ToList();
 
             }
 
