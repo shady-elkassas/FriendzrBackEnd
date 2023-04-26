d7965d276dce07f4c12168787d626e9dd8aef4b6
Alaa Farag <aabdelghany@youxel.com>
2023-04-26 11:14:30 +0200

    add migration AddLocationOptionsToMessageDataTable and update database

---
+++ Social.Entity/Migrations/20230426091324_AddLocationOptionsToMessageDataTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Social.Entity.Migrations
+{
+    public partial class AddLocationOptionsToMessageDataTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLiveLocation",
+                table: "Messagedata",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LocationPeriod",
+                table: "Messagedata",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LocationStartTime",
+                table: "Messagedata",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLiveLocation",
+                table: "Messagedata");
+
+            migrationBuilder.DropColumn(
+                name: "LocationPeriod",
+                table: "Messagedata");
+
+            migrationBuilder.DropColumn(
+                name: "LocationStartTime",
+                table: "Messagedata");
+        }
+    }
+}
--- Social.Entity/Models/Messagedata.cs
+++ Social.Entity/Models/Messagedata.cs
@@ -21,6 +21,9 @@ namespace Social.Entity.Models
         public string Longitude { get; set; }
         public string Latitude { get; set; }
         public string LocationName { get; set; }
+        public DateTime? LocationStartTime { get; set; }
+        public string LocationPeriod { get; set; }
+        public bool? IsLiveLocation { get; set; }
 
         public string MessagesAttached { get; set; }
         public bool linkable { get; set; }
